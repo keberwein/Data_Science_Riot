3febde7ef96577e99d1461dcb3edfc406ec5a344
keberwein77 <kristian.eberwein@yahoo.com>
2014-09-11 15:50:49 -0400

    added ISO analysis

---
+++ wISO_v_ISO/ISOpower_vs_ISO.R
@@ -0,0 +1,102 @@
+##Provides forulas for calculating the coefficient correlation of several baseball battig metrics.
+
+###The first section of code is used to gather the data from the Lahman database using a RMySQL connection.
+###I left the code so anyone who doen't have R connected directly to the dbase can still see the
+###required SQL code to gather the requried information.
+
+library(RMySQL)
+
+drv = dbDriver("MySQL")
+con = dbConnect(dbDriver("MySQL"), user = "root", password = "password", dbname = "lahman")
+
+##SQL get dat
+
+##Note the join on the table "Guts." This is a custom table that includes yearly wOBA values
+##The Guts table is only required for wOBA, you can delet the join and the wOBA calculation or
+##you can go to Fangraphs.com and download the Guts table to add to your own database.
+teams = dbSendQuery(con,
+                    "SELECT t.yearID, t.teamID, t.name,
+                    
+                    t.AB, t.H, t.2B, t.3B, t.HR, t.R, t.SB,
+                    
+                    t.H / t.AB AS BA,
+                    
+                    (t.H + t.BB + t.HBP) / (t.AB + t.BB + t.HBP + t.SF) AS OBP,
+                    
+                    ((t.H + t.BB + t.HBP) / (t.AB + t.BB + t.HBP + t.SF)) + (((t.H-t.2B-t.3B-t.HR) + (2 * t.2B) + (3 * t.3B) + (4 * t.HR))/t.AB) AS OPS,
+                    
+                    (t.H + t.2B + 2 * t.3B + 3 * t.HR) / t.AB AS SLG,
+                    
+                    (g.wBB * (t.BB) + g.wHBP * t.HBP + g.w1B * (t.H-t.2B-t.3B-t.HR) + g.w2B * t.2B + g.w3B * t.3B + g.wHR * t.HR) /
+                    (t.AB + t.BB + t.SF + t.HBP) AS wOBA,
+                    
+                    (t.2B) + (1.9*t.3B) + (3.17*t.HR) / t.AB AS wISO,
+                    
+                    (t.2B) + (2*t.3B) + (3*t.HR) / AB AS ISO
+                    
+                    FROM Teams t
+                    Join Guts g
+                    ON g.yearID = t.yearID
+                    WHERE t.yearID > 1901
+                    ")
+
+
+Batting = fetch(teams, n = -1)
+
+#Batting average
+plot(Batting$R, Batting$BA, xlab="Runs", 
+     ylab = "BA", pch=23, col='red')
+
+abline(lm(Batting$BA~Batting$R))
+
+cor(Batting$R,Batting$BA)
+
+#OBP
+
+plot(Batting$R, Batting$OBP, xlab="Runs", 
+     ylab = "OBP", pch=23, col='purple')
+
+abline(lm(Batting$OBP~Batting$R))
+
+cor(Batting$R,Batting$OBP)
+
+#Slugging 
+plot(Batting$R, Batting$SLG, xlab="Runs", 
+     ylab = "SLG", pch=23, col='green')
+
+abline(lm(Batting$SLG~Batting$R))
+
+cor(Batting$R,Batting$SLG)
+
+#OPS
+plot(Batting$R, Batting$OPS, xlab="Runs", 
+     ylab = "OPS", pch=23, col='blue')
+
+abline(lm(Batting$OPS~Batting$R))
+
+cor(Batting$R,Batting$OPS)
+
+#wOBA
+#Note, this wOBA calculation DOES NOT account for IBB but applies weights to the OPS formula.
+plot(Batting$R, Batting$wOBA, xlab="Runs", 
+     ylab = "wOBA", pch=23, col='brown')
+
+abline(lm(Batting$wOBA~Batting$R))
+
+cor(Batting$R,Batting$wOBA)
+
+#wISO
+plot(Batting$R, Batting$wISO, xlab="Runs", 
+     ylab = "wISO", pch=23, col='red')
+
+abline(lm(Batting$wISO~Batting$R))
+
+cor(Batting$R,Batting$wISO)
+
+#ISO
+plot(Batting$R, Batting$ISO, xlab="Runs", 
+     ylab = "ISO", pch=23, col='red')
+
+abline(lm(Batting$ISO~Batting$R))
+
+cor(Batting$R,Batting$ISO)
