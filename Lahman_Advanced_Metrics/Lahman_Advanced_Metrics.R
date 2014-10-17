2f71282cfe638cc38d8ebe66dbd540b6fb8cfa97
Keberwein <kristian.eberwein@yahoo.com>
2014-10-16 21:40:36 -0400

    Update Lahman_Advanced_Metrics.R
    Added round function

--- Lahman_Advanced_Metrics/Lahman_Advanced_Metrics.R
+++ Lahman_Advanced_Metrics/Lahman_Advanced_Metrics.R
@@ -36,48 +36,46 @@ f = fetch(fielding, n = -1)
 
 b$PA = (b$AB + b$BB+ b$HBP + b$SF + b$SH) #Plate Appearences
 
-b$OBP = ((b$H+b$BB+b$HBP)/(b$AB+b$BB+b$HBP+b$SF)) #On base %
+b$OBP = round(((b$H+b$BB+b$HBP)/(b$AB+b$BB+b$HBP+b$SF)), 3) #On base %
 
-b$SLG = ((b$H+b$'2B'+2*b$'3B'+3*b$HR)/b$AB) #Slugging
+b$SLG = round(((b$H+b$'2B'+2*b$'3B'+3*b$HR)/b$AB), 3) #Slugging
 
-b$ISO = (((b$'2B') + (2*b$'3B') + (3*b$'HR')) / b$AB) #Isolated Power
+b$ISO = round((((b$'2B') + (2*b$'3B') + (3*b$'HR')) / b$AB), 3) #Isolated Power
 
-b$OPS = ((b$H + b$BB + b$HBP) / (b$AB + b$BB + b$HBP + b$SF)) + (((b$H-b$'2B'-b$'3B'-b$HR) 
-        + (2 * b$'2B') + (3 * b$'3B') + (4 * b$HR))/b$AB) #On Base Plus Slugging
+b$OPS = round(((b$H + b$BB + b$HBP) / (b$AB + b$BB + b$HBP + b$SF)) + (((b$H-b$'2B'-b$'3B'-b$HR) 
+        + (2 * b$'2B') + (3 * b$'3B') + (4 * b$HR))/b$AB), 3) #On Base Plus Slugging
 
-b$RC = ((b$H+b$BB)*(b$H+b$2B+2*b$3B+3*b$HR))/(b$AB+b$BB) #Runs Created
+b$BABIP = round(((b$H-b$HR)/(b$PA-b$SO-b$BB-b$HR)), 3) #Batting Average on Balls in Play
 
-b$BABIP = ((b$H-b$HR)/(b$PA-b$SO-b$BB-b$HR)) #Batting Average on Balls in Play
+b$ContactRate = round(((b$AB-b$SO)/b$AB), 3) #Batter contact rate
 
-b$ContactRate = ((b$AB-b$SO)/b$AB) #Batter contact rate
+b$Kpct = round((b$SO/b$PA), 3) #Strikeout Rate 
 
-b$Kpct = (b$SO/b$PA) #Strikeout Rate 
-
-b$BBpct = (b$BB/b$PA) #Base on Balls rate
+b$BBpct = round((b$BB/b$PA), 3) #Base on Balls rate
 
 
 ##Pitching Calculate stats and add them to data frame.
 
-p$IP = ((p$IPouts)/3) #Innings Pitched
+p$IP = round((p$IPouts)/3), 3) #Innings Pitched
 
-p$WHIP = ((p$BB + p$H) / (p$IPouts/3))
+p$WHIP = round(((p$BB + p$H) / (p$IPouts/3)), 3)
 
-p$k_9 = (p$SO*9)/p$IP #Strikeouts per 9
+p$k_9 = round((p$SO*9)/p$IP, 3) #Strikeouts per 9
 
-p$BB_9 = (p$BB*9)/p$IP #Walks allowed per 9
+p$BB_9 = round((p$BB*9)/p$IP, 3) #Walks allowed per 9
 
-p$HR_9 = (p$HR*9)/p$IP #Homeruns allowed per 9
+p$HR_9 = round((p$HR*9)/p$IP, 3) #Homeruns allowed per 9
 
-p$Kpct = (p$SO/p$BFP)
+p$Kpct = round((p$SO/p$BFP), 3)
 
-p$BBpct = (p$BB/p$BFP)
+p$BBpct = round((p$BB/p$BFP), 3)
 
-p$pBABIP = ((p$H-p$HR)/(p$BFP-p$SO-p$BB-p$HR)) #Batting Average on Balls in Play
+p$pBABIP = round(((p$H-p$HR)/(p$BFP-p$SO-p$BB-p$HR)), 3) #Batting Average on Balls in Play
 
 
 ##Fielding Calculate stats and add them to data frame.
 
-f$FldPct = (f$PO+f$A)/(f$PO+f$A+f$E) #Fielding Percentage
+f$FldPct = round((f$PO+f$A)/(f$PO+f$A+f$E), 3) #Fielding Percentage
 
 
 ##Optionally you can export these data frames to csv or write them directly to you database with the following:
