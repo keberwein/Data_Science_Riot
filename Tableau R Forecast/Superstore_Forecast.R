4a2b375ecbbe86803644bfa352488db716538ae1
Keberwein <kristian.eberwein@yahoo.com>
2015-08-05 22:02:09 -0400

    added Arima forecast

--- Tableau R Forecast/Superstore_Forecast.R
+++ Tableau R Forecast/Superstore_Forecast.R
@@ -1,12 +1,15 @@
-library(forcast)
+library(forecast)
+
 #Date formatting is bad, convert to correct format
 superstore_fit$Order.Date <- as.Date(as.character(superstore_fit$Order.Date), format="%m/%d/%y")
 #Create time sereis
 ts <- ts(data=superstore_fit$Profit, start=c(2010, 1), deltat=1/12)
 #Fit into an ETS forecsast model
-fit <- ets(ts)
+fit <- Arima(ts)
+fit1 <- ets(ts)
 #Determine accuracy of fit
 accuracy(fit)
+accuracy(fit1)
 #Plot fitted model
 plot(forecast(ts))
 lines(fitted(fit),col="blue")
