a44326567c85694f5ec98d3a92a7d3fe0e58dcd7
Keberwein <kristian.eberwein@yahoo.com>
2015-09-01 16:28:17 -0400

    Added confidence intervals

--- Tableau R Forecast/Superstore_Forecast.R
+++ Tableau R Forecast/Superstore_Forecast.R
@@ -1,16 +1,13 @@
 library(forecast)
-
-#Date formatting is bad, convert to correct format
-superstore_fit$Order.Date <- as.Date(as.character(superstore_fit$Order.Date), format="%m/%d/%y")
-#Create time sereis
-ts <- ts(data=superstore_fit$Profit, start=c(2010, 1), deltat=1/12)
-#Fit into an ETS forecsast model
-fit <- Arima(ts)
-fit1 <- ets(ts)
-#Determine accuracy of fit
-accuracy(fit)
-accuracy(fit1)
-#Plot fitted model
-plot(forecast(ts))
-lines(fitted(fit),col="blue")
+# Date formatting is bad, convert to correct format
+SuperstoreSales$Order.Date <- as.Date(as.character(SuperstoreSales$Order.Date), format="%m/%d/%y")
+# Create time sereis
+time <- ts(data=SuperstoreSales$Sales, start=c(2010, 1), deltat=1/12)
+# Fit into forecsast model
+fcast <- forecast(time)
+# Determine accuracy of fit
+accuracy(fcast)
+# Plot fitted model
+plot(forecast(time))
+lines(fitted(fcast),col="blue")
 
