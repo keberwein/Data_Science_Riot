5d547fc9b69ce524bc5cbeecdd6668e47c5346b0
Keberwein <kristian.eberwein@yahoo.com>
2016-03-01 23:09:23 -0500

    Initial commit

---
+++ Sentiment_Analysis/super_tues.R
@@ -0,0 +1,107 @@
+pkgs <-c('twitteR','ROAuth','httr','plyr','stringr','ggplot2','plotly')
+for(p in pkgs) if(p %in% rownames(installed.packages()) == FALSE) {install.packages(p)}
+for(p in pkgs) suppressPackageStartupMessages(library(p, quietly=TRUE, character.only=TRUE))
+
+# Set API Keys
+api_key <- "HS5EygF5UzVkFZsR7YboHBk2L"
+api_secret <- "7ejuCMm8VhvTWGVJUfmIUrJ7Se1Fx66vuECrfExMqkaRacqNJx"
+access_token <- "482232051-riEfZIZAkT63mtSV31D1OTkt77o1Zj9CiX2w59Jg"
+access_token_secret <- "EIb9llWKeZXAiG2NmgcwdmvF0z7mi8cFC8LSGuauqmKnq"
+setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+
+# Grab latest tweets
+tweets_trump <- searchTwitter('@realDonaldTrump', n=5000)
+tweets_sanders <- searchTwitter('@BernieSanders', n=5000)
+tweets_clinton <- searchTwitter('@HillaryClinton', n=5000)
+tweets_cruz <- searchTwitter('@tedcruz', n=5000)
+
+# Loop over tweets and extract text
+feed_trump = laply(tweets_trump, function(t) t$getText())
+feed_sanders = laply(tweets_sanders, function(t) t$getText())
+feed_clinton = laply(tweets_clinton, function(t) t$getText())
+feed_cruz = laply(tweets_cruz, function(t) t$getText())
+
+# Read in dictionary of positive and negative workds
+yay = scan('opinion-lexicon-English/positive-words.txt',
+                  what='character', comment.char=';')
+boo = scan('opinion-lexicon-English/negative-words.txt',
+                  what='character', comment.char=';')
+# Add a few twitter-specific negative phrases
+bad_text = c(boo, 'wtf', 'wait', 'waiting',
+              'epicfail', 'slow')
+good_text = c(yay, 'upgrade', ':)', '#iVoted', 'voted')
+
+score.sentiment = function(sentences, good_text, bad_text, .progress='none')
+{
+    require(plyr)
+    require(stringr)
+    # we got a vector of sentences. plyr will handle a list
+    # or a vector as an "l" for us
+    # we want a simple array of scores back, so we use
+    # "l" + "a" + "ply" = "laply":
+    scores = laply(sentences, function(sentence, good_text, bad_text) {
+        
+        # clean up sentences with R's regex-driven global substitute, gsub():
+        sentence = gsub('[[:punct:]]', '', sentence)
+        sentence = gsub('[[:cntrl:]]', '', sentence)
+        sentence = gsub('\\d+', '', sentence)
+        sentence = tolower(sentence)
+        
+        # split into words. str_split is in the stringr package
+        word.list = str_split(sentence, '\\s+')
+        # sometimes a list() is one level of hierarchy too much
+        words = unlist(word.list)
+        
+        # compare our words to the dictionaries of positive & negative terms
+        pos.matches = match(words, good_text)
+        neg.matches = match(words, bad_text)
+        
+        # match() returns the position of the matched term or NA
+        # we just want a TRUE/FALSE:
+        pos.matches = !is.na(pos.matches)
+        neg.matches = !is.na(neg.matches)
+        
+        # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
+        score = sum(pos.matches) - sum(neg.matches)
+        
+        return(score)
+    }, good_text, bad_text, .progress=.progress )
+    
+    scores.df = data.frame(score=scores, text=sentences)
+    return(scores.df)
+}
+
+# Retreive scores and add candidate name.
+thedonald <- score.sentiment(feed_trump, good_text, bad_text, .progress='text')
+thedonald$name <- 'Trump'
+feelthabern <- score.sentiment(feed_sanders, good_text, bad_text, .progress='text')
+feelthabern$name <- 'Sanders'
+clinton <- score.sentiment(feed_clinton, good_text, bad_text, .progress='text')
+clinton$name <- 'Clinton'
+cruz <- score.sentiment(feed_cruz, good_text, bad_text, .progress='text')
+cruz$name <- 'Cruz'
+# Merge into one dataframe for plotting
+plotdat <- rbind(thedonald, feelthabern, clinton, cruz)
+# Cut the text, just gets in the way
+plotdat <- plotdat[c("name", "score")]
+# Remove neutral values of 0
+plotdat <- plotdat[!plotdat$score == 0, ]
+
+# Nice little quick plot
+qplot(factor(score), data=plotdat, geom="bar", 
+      fill=factor(name),
+      xlab = "Sentiment Score")
+
+# Or get funky with ggplot2 + Plotly
+ep <- plotdat %>%
+    ggplot(aes(x = score, fill = name)) +
+    geom_bar(binwidth = 1) +
+    scale_fill_manual(values = c("#0067F7","#7B00F7", "#7CF700", "#F70000")) +
+    theme_classic(base_size = 12) +
+    scale_x_continuous(name = "Sentiment Score") +
+    scale_y_continuous(name = "Text count of tweets") +
+    ggtitle("Super Tuesday Twitter Sentiment: 2016")
+    theme(axis.title.y = element_text(face="bold", colour="#000000", size=10),
+          axis.title.x = element_text(face="bold", colour="#000000", size=8),
+          axis.text.x = element_text(angle=16, vjust=0, size=8))
+ggplotly(ep)
