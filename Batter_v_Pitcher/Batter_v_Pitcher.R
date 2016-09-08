c619c134855488224de8d81c509d7c2863b78d8d
Keberwein <kristian.eberwein@yahoo.com>
2016-09-08 16:55:23 -0400

    removed caps

--- Batter_v_Pitcher/Batter_v_Pitcher.R
+++ Batter_v_Pitcher/Batter_v_Pitcher.R
@@ -16,46 +16,46 @@ atbat08 <- filter(tbl(db, 'atbat'), date >= '2008_01_01' & date <= '2013_01_01')
 explain(atbat08)
 
 #Create list for player
-Lee_C_08 <- filter(atbat08, pitcher_name == 'Cliff Lee')
+lee08 <- filter(atbat08, pitcher_name == 'Cliff Lee')
 
 #Join atbat and pitch tables
 pitches <- tbl(db, 'pitch')
-Lee08 <- inner_join(pitches, Lee_C_08, by = c('num', 'gameday_link'))
+lee08 <- inner_join(pitches, lee08, by = c('num', 'gameday_link'))
 
 #Collect Data
-Cliff_Lee_08 <- collect(Lee08)
+cliffLee08 <- collect(lee08)
 
 #See Summary
-summary(Cliff_Lee_08)
+summary(cliffLee08)
 
 ####################################################
 #Pitch Types
 #Create subset with differant pitch types
-LeePtype <- subset(Cliff_Lee_08, !(pitch_type  %in%  c("IN",  "")))
+LeePtype <- subset(cliffLee08, !(pitch_type  %in%  c("IN",  "")))
 
 #Find Pitch type and Stand
-Cliff_Lee_08 %.%
+cliffLee08 %.%
   group_by(pitch_type, stand) %>%
   summarise(count = n()) %>%
   arrange(desc(count))
 
 #Plot pitch types
 ggplot(data = LeePtype) +
-  stat_density2d(geom="tile", aes(x = px, y = pz, fill = ..density..), contour = F, data = Cliff_Lee_08) +
+  stat_density2d(geom="tile", aes(x = px, y = pz, fill = ..density..), contour = F, data = cliffLee08) +
   facet_wrap(~pitch_type) +
   scale_x_continuous("horizontal pitch location") +
   scale_y_continuous("vertical pitch location") +
   coord_cartesian(xlim = c(-2, 2), ylim = c(1, 4))
 
 #Plot pitches near strikezone
-strikeFX(Cliff_Lee_08, color="", layer=facet_grid(s~stand))+
+strikeFX(cliffLee08, color="", layer=facet_grid(s~stand))+
   geom_point(aes(x=px, y=pz, shape=pitch_types))+ 
   geom_text(aes(x=px+0.5, y=pz, label=b))
 
-p <- strikeFX(Cliff_Lee_08, geom="tile", layer=facet_grid(.~stand))
+p <- strikeFX(cliffLee08, geom="tile", layer=facet_grid(.~stand))
 p+theme(aspect.ratio=1)
 
-strikeFX(Cliff_Lee_08, geom="hex", density1=list(des="Called Strike"), density2=list(des="Ball"),
+strikeFX(cliffLee08, geom="hex", density1=list(des="Called Strike"), density2=list(des="Ball"),
          draw_zones=FALSE, contour=TRUE, layer=facet_grid(.~stand))
 noswing <- subset(pitches, des %in% c("Ball", "Called Strike"))
 
@@ -67,11 +67,11 @@ m1 <- bam(strike ~ s(px, pz, by=factor(stand)) +
 strikeFX(noswing, model=m1, layer=facet_grid(.~stand))
 
 #Plot Strikes
-strikes <- subset(Cliff_Lee_08, des == "Called Strike")
+strikes <- subset(cliffLee08, des == "Called Strike")
 strikeFX(strikes, geom="tile", layer=facet_grid(.~stand))
 
 #Against a Specific Batter
-Lee_v_Mags_08 <- subset(Cliff_Lee_08, batter_name == "Miguel Cabrera")
+Lee_v_Mags_08 <- subset(cliffLee08, batter_name == "Miguel Cabrera")
 MagsPitchTypes <- subset(Lee_v_Mags_08, !(pitch_type  %in%  c("IN",  "")))
 MagsStrikes <- subset(Lee_v_Mags_08, des == "Called Strike")
 
