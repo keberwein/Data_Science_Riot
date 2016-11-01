54aef1b7c66743331420da68b941c47cfb2d7b4f
Keberwein <kristian.eberwein@yahoo.com>
2016-11-01 16:23:36 -0400

    syntax

--- Forecast/Forecast.R
+++ Forecast/Forecast.R
@@ -2,31 +2,30 @@ require(forecast)
 
 ###Forecast the Gross Domestic Product
 
-#Set date type
+# Set date type
 gdp$Date <- as.Date(as.character(gdp$Date), "%Y-%m-%d")
 
-#Set GDP numeric type
+# Set GDP numeric type
 gdp$GDP <- as.numeric(as.character(gdp$GDP))
 
-#Create time series vector from data frame
+# Create time series vector from data frame
 gdpts <- ts(data=gdp$GDP, start=c(1947, 1), deltat=3/12)
 
-#Forecast and Plot
+# Forecast and Plot
 plot(forecast.ets(gdpts))
 
 summary(forecast(gdpts))
 
 
-
 ###Forecast Major League Homeruns
 
-#Set numeric data type
+# Set numeric data type
 HR$Homeruns <- as.numeric(as.character(HR$Homeruns))
 
-#Create time series vector from data frame
+# Create time series vector from data frame
 hrts <- ts(data=HR$Homeruns, start=c(1947, 1))
 
-#Forecast and Plot
+# Forecast and plot
 plot(forecast(hrts), main="Homeruns")
 
 summary(forecast(hrts))
